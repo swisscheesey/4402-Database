fb21e2b80b09a8f74c3f22d10f8279759e06dd58
swisscheesey <thiccpfsognr@gmail.com>
2023-11-29 21:43:01 -0600

    skeleton code setup

---
+++ gsim-backend/src/main/java/com/gsim/gsimbackend/GsimBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gsim.gsimbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsimBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsimBackendApplication.class, args);
+	}
+
+}
---
+++ gsim-backend/src/test/java/com/gsim/gsimbackend/GsimBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsim.gsimbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsimBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
