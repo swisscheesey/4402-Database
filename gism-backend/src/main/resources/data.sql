661fe0a5dc2db3ec630877349e4eecbb4eb360b1
Joshua Villere <joshvill234@gmail.com>
2023-11-30 21:26:20 -0600

    data update

--- gism-backend/src/main/resources/data.sql
+++ gism-backend/src/main/resources/data.sql
@@ -1,4 +1,37 @@
 
 INSERT INTO item (product_ID, product_name, price, brand)
-VALUES (1, 'Chips', 90000.00, 'Lays');
+VALUES (1, 'Potato Chips', 2.00, 'Air-Chips');
+VALUES (2, 'Cheddar Chips', 3.50, 'Air-Chips');
+VALUES (3, 'Tortilla Chips', 5.00, 'Evan Snacks');
+VALUES (4, 'Cola', 1.20, 'Falso');
+VALUES (5, 'Lemon-Lime Soda', 1.20, 'Redbeak');
+VALUES (6, 'Chocolate Milk', 1.50, 'Gulpy');
+VALUES (7, 'Chocolate Chip Cookies', 2.00, 'Kippies');
+VALUES (8, 'Chocolate Bar', 1.75, 'Evan Snacks');
+VALUES (9, 'Chocolate Bar', 1.60, 'Kippies');
+VALUES (10, 'Hot Dog', 3.25, 'Circle K');
+VALUES (11, 'Chicken Sandwich', 3.00, 'Circle K');
+VALUES (12, 'Potato Chips', 2.00, 'Air-Chips');
+VALUES (13, 'Potato Chips', 2.00, 'Air-Chips');
+VALUES (14, 'Potato Chips', 2.00, 'Air-Chips');
+VALUES (15, 'Potato Chips', 2.00, 'Air-Chips');
+VALUES (16, 'Potato Chips', 2.00, 'Air-Chips');
+VALUES (17, 'Potato Chips', 2.00, 'Air-Chips');
+VALUES (18, 'Potato Chips', 2.00, 'Air-Chips');
+VALUES (19, 'Potato Chips', 2.00, 'Air-Chips');
+VALUES (20, 'Potato Chips', 2.00, 'Air-Chips');
+VALUES (21, 'Potato Chips', 2.00, 'Air-Chips');
+VALUES (22, 'Potato Chips', 2.00, 'Air-Chips');
+VALUES (23, 'Potato Chips', 2.00, 'Air-Chips');
+VALUES (24, 'Potato Chips', 2.00, 'Air-Chips');
+VALUES (25, 'Potato Chips', 2.00, 'Air-Chips');
+VALUES (26, 'Potato Chips', 2.00, 'Air-Chips');
+VALUES (27, 'Potato Chips', 2.00, 'Air-Chips');
+VALUES (28, 'Potato Chips', 2.00, 'Air-Chips');
+VALUES (29, 'Potato Chips', 2.00, 'Air-Chips');
+VALUES (30, 'Potato Chips', 2.00, 'Air-Chips');
+VALUES (31, 'Potato Chips', 2.00, 'Air-Chips');
+VALUES (32, 'Potato Chips', 2.00, 'Air-Chips');
+VALUES (33, 'Potato Chips', 2.00, 'Air-Chips');
+VALUES (34, 'Potato Chips', 2.00, 'Air-Chips');
 
